c84fdf482c12256fe9d0dcda2e35fbfd4bd0330f
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-08-13 10:16:11 +0100

    Local database class

---
+++ v0.1/assets/models/db.php
@@ -0,0 +1,26 @@
+<?php
+namespace Database;
+
+use PDO;
+use PDOException;
+class Database {
+
+    static $DB_HOST = "localhost";
+    static $DB_NAME = "test";
+    static $DB_USER = "root";
+    static $DB_PASS = '';
+
+    static $connection;
+
+    public static function getConnection() {
+        self::$connection = null;
+        try {
+            self::$connection = new PDO("mysql:host=" . self::$DB_HOST . ";dbname=" . self::$DB_NAME, self::$DB_USER, self::$DB_PASS);
+            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return self::$connection;
+    }
+}
+?>
