daed46282fdecb6d1f3e60d53c802a1ffb957762
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-08-15 23:36:22 +0100

    Signup/Register System

--- v0.1/assets/controllers/auth.php
+++ v0.1/assets/controllers/auth.php
@@ -8,27 +8,80 @@ use PDOException; //Change later
 class CAuth {
 
     // User login 
-    public static function userLogin (...$data) // Email and password
+    public static function userLogin ($data) // Email and password
     {
         try {
-            if (sizeof($data) < 2 ){
-                return json_decode(CUtils::returnData(false, "Data is Missing", $data, true));
-            }
+            // if (sizeof($data) < 2 ){
+            //     return json_decode(CUtils::returnData(false, "Data is Missing", $data, true));
+            // }
 
-            $checkUser = json_decode(MAuth::checkUserMail($data[0])); // $data[0] is email
+            $checkUser = json_decode(MAuth::checkUserMail($data->email)); // $data[0] is email
             if ($checkUser->status == false) {
-                return json_decode(CUtils::returnData($checkUser->status, "Please Signup", $data[0], true));
+                return json_decode(CUtils::returnData($checkUser->status, "Please Signup", $data, true));
             }
 
             if ($checkUser->status == true) {
-                if ($data[1] === $checkUser->data->password) {
-                    return json_decode(CUtils::returnData(true, "Logged In", $checkUser->data->token, true));
+                if ($data->password === $checkUser->data->password) {
+                    return json_decode(CUtils::returnData(true, "Logged In", $checkUser->data->id, true));
                 } else {
                     return json_decode(CUtils::returnData(false, "Password Incorrect", $data, true));
                 }
             }
         } catch (PDOException $e) {
             return CUtils::returnData(false, $e->getMessage(), $data, true);
+        }
+    }
+
+
+    // User Signup
+    public static function userSignup ($data) // Email, Password, Confirm password, First name, Last name as an object for validation of correct input
+    {
+        try{
+            // if ($data == null) {
+            //     return CUtils::returnData(false, "No data found", $data, true);
+            // }
+            $checkMail = json_decode(MAuth::checkMail($data->email));
+            if ($checkMail->status == false) {
+                return json_decode(CUtils::returnData($checkMail->status, "This email is Registered", $data->email, true));
+            }
+            $validMail = json_decode(CUtils::validateEmail($data->email));
+            if ($validMail->status == false) {
+                return json_decode(CUtils::returnData($validMail->status, $validMail->message, $data, true));
+            }
+            $email = $data->email;
+
+            if (strlen($data->password) < 8) {
+                return json_decode(CUtils::returnData(false, "Password cannot be less than 8 characters", $data->password, true));
+            }
+            if (!preg_match('~[0-9]+~', $data->password)) {
+                return json_decode(CUtils::returnData(false, "Password must contain a number", $data, true));
+            }
+            if (!preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $data->password)) {
+                return json_decode(CUtils::returnData(false, "Password must contain character", $data, true));
+            }
+            if ($data->password != $data->cpassword) {
+                return json_decode(CUtils::returnData(false, "Password does not match", $data, true));
+            }
+            $password = password_hash($data->password, PASSWORD_DEFAULT);
+
+            $firstname = trim($data->first_name);
+            $lastname = trim($data->last_name);
+            if (!preg_match("/^[a-zA-Z'-]+$/", $firstname)) {
+                return json_decode(CUtils::returnData(false, "Invalid First name format", $data, true));
+            }
+            if (!preg_match("/^[a-zA-Z'-]+$/", $lastname)) {
+                return json_decode(CUtils::returnData(false, "Invalid Last name format", $data, true));
+            }
+
+            $signupUser = json_decode(MAuth::userSignup($email, $password, $firstname, $lastname));
+            if ($signupUser->status == false) {
+                return json_decode(CUtils::returnData(false, $signupUser->message, $signupUser->data, true));
+            }
+        
+            return json_decode(CUtils::returnData(true, "Registration successful", $data, true));
+
+        } catch (PDOException $e) {
+
         }
     }
 }
--- v0.1/assets/models/auth.php
+++ v0.1/assets/models/auth.php
@@ -9,11 +9,11 @@ use PDOException; // Change later
 
 class MAuth {
 
-    //  Check and get details with email 
+    //  Check and get details with email - Login 
     public static function checkUserMail ($email)
     {
         try {
-            $query = "SELECT token, password FROM users where email = :email";
+            $query = "SELECT id,  password FROM users WHERE email = :email";
             $stmt = Database::getConnection()->prepare($query);
 
             $stmt->bindParam(':email', $email, PDO::PARAM_STR);
@@ -31,5 +31,59 @@ class MAuth {
         } catch (PDOException $e) {
             return CUtils::returnData(false, $e->getMessage(), $email, true);
         }
-    }
+    } // End of method 
+
+
+    // Check if email doesn't exist and can be used
+    public static function checkMail($email)
+    {
+        try {
+            $query = "SELECT id FROM users_info WHERE email = :email";
+            $stmt = Database::getConnection()->prepare($query);
+
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            if(!$stmt->execute()){
+                exit;
+            }
+
+            if ($stmt->rowCount() > 0){
+                return CUtils::returnData(false, null, $email, false); // Email exist - can't use
+            } else {
+                return CUtils::returnData(true, null, $email, false); // Email doesn't exist - can use
+            }
+
+        } catch (PDOException $e) {
+            return CUtils::returnData(false, $e->getMessage(), null, true);
+        }
+    } // End of method
+
+
+    // User Signup
+    public static function userSignup (...$data) // Email, Password, First name, Last name
+    {
+        try {
+            $query ="BEGIN;
+                    INSERT INTO users_info (email, password) 
+                        VALUES (:email, :password); 
+                    INSERT INTO users_details (user_id, first_name, last_name) 
+                        VALUES (LAST_INSERT_ID(),:first_name,:last_name); 
+                    COMMIT;";
+            $stmt = Database::getConnection()->prepare($query);
+
+            $stmt->bindParam(':email', $data[0], PDO::PARAM_STR);
+            $stmt->bindParam(':password', $data[1]);
+            $stmt->bindParam(':first_name', $data[2], PDO::PARAM_STR);
+            $stmt->bindParam(':last_name', $data[3], PDO::PARAM_STR);
+
+
+            if(!$stmt->execute()){
+                return CUtils::returnData(false, "Something went wrong, Try again", $data, true);
+            } 
+            return CUtils::returnData(true, "Account created", $data, true);
+
+        } catch (PDOException $e) {
+            return CUtils::returnData(false, "Something went wrong: " . $e->getMessage(), [], true);
+        }
+    }  // End of method
+
 }
