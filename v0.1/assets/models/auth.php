88f23c34f6cf2a53f8aedf82d58ea73263a2fb89
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-08-18 09:05:39 +0100

    Optimized signup method

--- v0.1/assets/models/auth.php
+++ v0.1/assets/models/auth.php
@@ -59,31 +59,37 @@ class MAuth {
 
 
     // User Signup
-    public static function userSignup (...$data) // Email, Password, First name, Last name
+    public static function userSignup($email, $password, $first_name, $last_name)
     {
+        $conn = Database::getConnection();
         try {
-            $query ="BEGIN;
-                    INSERT INTO users_info (email, password) 
-                        VALUES (:email, :password); 
-                    INSERT INTO users_details (user_id, first_name, last_name) 
-                        VALUES (LAST_INSERT_ID(),:first_name,:last_name); 
-                    COMMIT;";
-            $stmt = Database::getConnection()->prepare($query);
-
-            $stmt->bindParam(':email', $data[0], PDO::PARAM_STR);
-            $stmt->bindParam(':password', $data[1]);
-            $stmt->bindParam(':first_name', $data[2], PDO::PARAM_STR);
-            $stmt->bindParam(':last_name', $data[3], PDO::PARAM_STR);
-
-
-            if(!$stmt->execute()){
-                return CUtils::returnData(false, "Something went wrong, Try again", $data, true);
-            } 
-            return CUtils::returnData(true, "Account created", $data, true);
-
+            $conn->beginTransaction();
+    
+            $stmt1 = $conn->prepare("INSERT INTO users_info (email, password) VALUES (:email, :password)");
+            $stmt1->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt1->bindParam(':password', $password);
+            if (!$stmt1->execute()) {
+                throw new PDOException("Failed to insert into users_info");
+            }
+    
+            $user_id = $conn->lastInsertId();
+    
+            $stmt2 = $conn->prepare("INSERT INTO users_details (user_id, first_name, last_name) VALUES (:user_id, :first_name, :last_name)");
+            $stmt2->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt2->bindParam(':first_name', $first_name, PDO::PARAM_STR);
+            $stmt2->bindParam(':last_name', $last_name, PDO::PARAM_STR);
+            if (!$stmt2->execute()) {
+                throw new PDOException("Failed to insert into users_details");
+            }
+    
+            $conn->commit();
+            return CUtils::returnData(true, "Account created", [], true);
         } catch (PDOException $e) {
+            // Rollback the transaction if something went wrong
+            $conn->rollBack();
             return CUtils::returnData(false, "Something went wrong: " . $e->getMessage(), [], true);
         }
-    }  // End of method
+    }
+      // End of method
 
 }
