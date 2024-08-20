e0649be11185691950b7be08774625668f5d1d77
sam-uel-ayo <samuelayomide0705@gmail.com>
2024-08-20 03:26:03 +0100

    Change password & Edit profile

--- v0.1/assets/models/auth.php
+++ v0.1/assets/models/auth.php
@@ -9,7 +9,7 @@ use PDOException; // Change later
 
 class MAuth {
 
-    //  Check and get details with email - Login 
+    //  Check and get details with email - Login/change password 
     public static function checkUserMail ($email)
     {
         try {
@@ -95,6 +95,86 @@ class MAuth {
             return CUtils::returnData(false, "Something went wrong: " . $e->getMessage(), [], true);
         }
     }
-      // End of method
+    // End of method
+
+
+    // Get User Personal information
+    public static function userProfile($token)
+    {
+        try {
+            $query = "SELECT * FROM users_details WHERE token = :token";
+            $stmt = Database::getConnection()->prepare($query);
+
+            $stmt->bindParam(':$token', $token, PDO::PARAM_STR);
+            if(!$stmt->execute()){
+                return CUtils::returnData(false, "Something went wrong, try again", null, true);
+            }
+
+            if ($stmt->rowCount() < 1){
+                return CUtils::returnData(false, "Profile not found", $token, false); // No profile found - Not possible though
+            } 
+
+            $profile = $stmt->fetch(PDO::FETCH_ASSOC);
+            return CUtils::returnData(true, "User found", $profile, false); // 
+            
+        } catch (PDOException $e) {
+            return CUtils::returnData(false, $e->getMessage(), null, true);
+        }
+    }
+    // End method
+
+
+    // Edit/Add User infomation
+    public static function editProfile ($firstname, $lastname, $dob, $phonenumber, $token)
+    {
+        // $editProfile = [$firstname, $lastname, $dob, $phonenumber, $token];
+        // return CUtils::returnData(false, "Data is here model", $editProfile, true);
+        // //return CUtils::returnData(true, "Data is here model", $editProfile, true);
+        try {
+            $query = "UPDATE users_details SET first_name=:firstname, last_name=:lastname, dob=:dob, phone_number=:phonenumber WHERE token=:token";
+            $stmt = Database::getConnection()->prepare($query);
+
+            $stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR);
+            $stmt->bindParam(':lastname', $lastname, PDO::PARAM_STR);
+            $stmt->bindParam(':phonenumber', $phonenumber, PDO::PARAM_STR);
+            $stmt->bindParam(':dob', $dob, PDO::PARAM_STR);
+            $stmt->bindParam(':token', $token);
+
+            if (!$stmt->execute()) {
+                return CUtils::returnData(false, "Failed to update, try again", $token, true);
+            }
+
+            return CUtils::returnData(true, "Profile Updated", $token, true);
+
+        } catch (PDOException $e) {
+            return CUtils::returnData(false, $e->getMessage(), $token, true);
+        }
+    }
+    // Method End
+
+
+    // Change password
+    public static function changePassword ($email, $password) 
+    {
+        try {
+            $query = "UPDATE users_info SET password=:password WHERE email=:email";
+            $stmt = Database::getConnection()->prepare($query);
+
+            $stmt->bindParam(':password', $password);
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+
+            if (!$stmt->execute()) {
+                return CUtils::returnData(false, "Something went wrong, try again", $email, true);
+            }
+
+            return CUtils::returnData(true, "Password changed", true);
+
+
+        } catch (PDOException $e) {
+            return CUtils::returnData(false, $e->getMessage(), $email, true);
+        }
+    }
+    // Method End
 
 }
